a4e80eea6641fae3a75fc7792fccc1a1592fdde9
mjaminbook <bmathers@macalester.edu>
2016-12-01 14:37:08 -0600

    added Category model

--- bidly_project/bidly_app/admin.py
+++ bidly_project/bidly_app/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Auction, Bidly_User, Role, Item, Bid
+from .models import Auction, Bidly_User, Role, Item, Bid, Category
 
 admin.site.register(Auction)
 admin.site.register(Bidly_User)
 admin.site.register(Role)
 admin.site.register(Item)
 admin.site.register(Bid)
+admin.site.register(Category)
--- bidly_project/bidly_app/models.py
+++ bidly_project/bidly_app/models.py
@@ -11,7 +11,6 @@ class Auction(models.Model):
 
 class Bidly_User(models.Model):
 	user = models.OneToOneField(User)
-	# user = models.ForeignKey(User, on_delete=models.CASCADE)
 	phone_number = models.CharField(max_length=11)
 
 	# def __str__(self):
@@ -22,13 +21,16 @@ class Role(models.Model):
 	user = models.ForeignKey(Bidly_User, on_delete=models.CASCADE)
 	role = models.ForeignKey(Group, on_delete=models.CASCADE)
 
+class Category(models.Model):
+	category_name = models.CharField(max_length=30)
+	
 class Item(models.Model):
 	auction = models.ForeignKey(Auction, on_delete=models.CASCADE)
 	starting_price = models.IntegerField(default=0)
 	increment = models.IntegerField(default=1)
 	name = models.CharField(max_length=30)
 	image_path = models.CharField(max_length=100)
-	# picture = models.ImageField(upload_to='item_images', blank=True)
+	category = models.ForeignKey(Category, on_delete=models.PROTECT)
 	value = models.IntegerField(default=0)
 	description = models.CharField(max_length=500)
 
@@ -40,4 +42,3 @@ class Bid(models.Model):
 	user = models.ForeignKey(Bidly_User, on_delete=models.CASCADE)
 	timestamp = models.DateTimeField(auto_now_add=True)
 	amount = models.IntegerField(default=0)
-
