2de3e4e6ec7940a720bf33db81de438aee7d26a7
Valenti <cvalenti@macalester.edu>
2016-12-13 16:59:46 -0600

    Added comments to Login

--- bidly_project/bidly_app/static/JS/login.js
+++ bidly_project/bidly_app/static/JS/login.js
@@ -1,4 +1,7 @@
 $(document).ready(function(e){
+	/*
+		Handles different styles of putting in phone numbers when creating an account. 
+	*/
 	$(".phone_num").keydown(function(e){
 		console.log("Keydown");
 		var input = e.target || e.srcElement;
