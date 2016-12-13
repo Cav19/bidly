cc442e43c7c2403f7d33d8d2ae89d8e92d869090
Valenti <cvalenti@macalester.edu>
2016-12-13 17:07:44 -0600

    Added comments in home.js

--- bidly_project/bidly_app/static/JS/home.js
+++ bidly_project/bidly_app/static/JS/home.js
@@ -15,6 +15,11 @@ $(document).ready(function(){
 		window.location.href = "/item_page/?item_id=" + this.id;
 	});
 
+	/*
+		Completes Ajax GET request for searching for an item. 
+		Success: Takes you to the item page for the item you searched. 
+		Error: Displays error. 
+	*/
 	$("#search-button").click(function() {
 		search_term = $('#search-term').val();
 		$.ajax({
@@ -27,6 +32,10 @@ $(document).ready(function(){
 		});
 	});
 });
+
+/*
+	Gets the top bid for each item displayed on the home page through and Ajax GET request. 
+*/
 function getItemBids(){
 	var items = document.getElementsByClassName("item");
 	// TODO: Update this for loop to not get each of the items more than once if they're displayed more than once on the page. 
@@ -42,6 +51,9 @@ function getItemBids(){
 	}
 }
 
+/*
+	Updates the current bid on each item on the page according to the top bid stored in the database.  
+*/
 var updateTopBid = function(data){
 	if(data.current_bid != null){
 		for(i = 0; i < $("." + data.item_id).length; i++){
